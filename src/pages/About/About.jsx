6419cffc42708d1e07bff4d6d968a34b46df7aaa
My Name <aakarshpatel84@gmail.com>
2023-06-01 01:30:41 +0530

    Added dummy data to about page & order page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,89 @@
 import React from "react";
-import "./About.css";
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div>
+        <div>
+          <h2>
+            Welcome to our online store! We're delighted to have you here. At
+            Aakarsh Store, we are passionate about delivering an exceptional
+            shopping experience to our valued customers.
+          </h2>
+        </div>
+
+        <div>
+          <h3>Our Story:</h3>
+          <p>
+            Established in 2022, we embarked on a journey to create a platform
+            that offers a wide range of high-quality products to meet your
+            diverse needs. With a team of dedicated professionals, we set out to
+            curate a collection of products that reflect the latest trends,
+            technological advancements, and customer preferences
+          </p>
+        </div>
+
+        <div>
+          <h3>Our Mission:</h3>
+          <p>
+            Our mission is to provide you with a seamless online shopping
+            experience, where you can discover an extensive selection of
+            products that cater to various interests and lifestyles. We strive
+            to exceed your expectations by offering superior customer service,
+            secure payment options, and prompt delivery, ensuring your
+            satisfaction from start to finish.
+          </p>
+        </div>
+
+        <div>
+          <h3> Product Selection:</h3>
+          <p>
+            Product Selection: At Aakarsh Store, we take pride in offering a
+            vast array of products to suit every individual. From electronics
+            and gadgets to fashion, home decor, beauty, and more, we strive to
+            bring you the best brands and products at competitive prices. We
+            constantly update our inventory to stay ahead of the curve and
+            provide you with the latest and most sought-after items.
+          </p>
+        </div>
+
+        <div>
+          <h3> Customer Satisfaction:</h3>
+          <p>
+            Your satisfaction is our top priority. We understand the importance
+            of a smooth and hassle-free shopping experience, which is why we
+            have a dedicated customer support team ready to assist you with any
+            inquiries, concerns, or feedback you may have. We value your trust
+            and aim to build long-lasting relationships with our customers based
+            on transparency, reliability, and exceptional service.
+          </p>
+        </div>
+        <div>
+          <h3> Secure Shopping:</h3>
+          <p>
+            We take your online security seriously. Our website is equipped with
+            state-of-the-art encryption technology to ensure that your personal
+            information and payment details are safeguarded at all times. You
+            can shop with confidence knowing that your privacy is our utmost
+            concern.
+          </p>
+        </div>
+
+        <div>
+          <p>
+            Thank you for choosing Aakarsh Store as your go-to online shopping
+            destination. We hope you enjoy exploring our vast selection of
+            products and find exactly what you're looking for. If you have any
+            questions or need assistance, please don't hesitate to contact our
+            friendly customer support team.
+          </p>
+        </div>
+
+        <div>
+          <p>Happy shopping! Aakarsh Store Team</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Order.css";
+import { AiTwotoneStar } from "react-icons/ai";
+import axios from "axios";
 function Order() {
-  return <div>Order</div>;
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => setData(res.data))
+      .catch((err) => err.message);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div className="parent">
+        {data.map((item) => (
+          <div>
+            <img src={item.image} alt="" />
+            <p>Title: {item.title} </p>
+            <p> {item.rating.rate}</p>
+            {/* <span>
+              <AiTwotoneStar color="yellow" size="20px" />
+            </span> */}
+            <h3>Price: {item.price}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Order;
