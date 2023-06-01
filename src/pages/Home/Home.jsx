2b5d0acee458382c81fa8074c1056af18a374097
My Name <aakarshpatel84@gmail.com>
2023-06-01 10:46:15 +0530

    Implemented filter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { setProducts } from "../../app/globalSlice";
 
 function Home() {
+  const [categories, setCategories] = useState([]);
+  const [selected, setSelected] = useState([]);
   const dispatch = useDispatch();
   const data = useSelector((store) => store.global.products);
   const getData = () => {
@@ -12,33 +14,75 @@ function Home() {
       .then((res) => dispatch(setProducts(res.data)))
       .catch((err) => err.message);
   };
+  const getCategories = () => {
+    axios
+      .get("https://fakestoreapi.com/products/categories")
+      .then((res) => setCategories(res.data))
+      .catch((err) => err.message);
+  };
   useEffect(() => {
     getData();
+    getCategories();
   }, []);
 
+  const filteredProducts = selected.length
+    ? data.filter(({ category }) => selected.includes(category))
+    : data;
+
   return (
-    <div className="container p-2">
-      <div className="row gutter-2">
-        {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
-        {data.map((item) => (
-          <div className="col-3 my-2">
-            <div class="card h-100">
-              <img class="card-img-top h-100" src={item.image} alt="Card cap" />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
+    <>
+      <div class="d-flex p-2 justify-content-center">
+        <div>
+          {categories.map((category) => (
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                // value=""
+                id={category}
+                value={category}
+                checked={selected.includes(category)}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setSelected((sel) => [...sel, category]);
+                  } else {
+                    setSelected((sel) =>
+                      sel.filter((item) => item !== category)
+                    );
+                  }
+                }}
+              />
+              <label class="form-check-label" for={category}>
+                {category}
+              </label>
             </div>
+          ))}
+        </div>
+        <div className="container p-2">
+          <div className="row gutter-2">
+            {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
+            {filteredProducts.map((item) => (
+              <div className="col-3 my-2">
+                <div class="card h-100">
+                  <img
+                    class="card-img-top h-100"
+                    src={item.image}
+                    alt="Card cap"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">{item.title}</h5>
+                    <p class="card-text">${item.price}</p>
+                    <button href="#" class="btn btn-primary">
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
