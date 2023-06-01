64756692544074f795727eacc067c3b118bcd731
My Name <aakarshpatel84@gmail.com>
2023-06-01 13:43:34 +0530

    Completed Details page

--- src/pages/ProductDetailPage/ProdeuctDetailPage.jsx
+++ src/pages/ProductDetailPage/ProdeuctDetailPage.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-
+import "./productDetail.css";
 function ProdeuctDetailPage() {
   const { productId } = useParams();
   const data = useSelector((store) => store.global.products);
   let product = data.find((el) => el.id === +productId);
-  console.log("productId: ", product);
-  return <div>ProdeuctDetailPage</div>;
+  return (
+    <>
+      <div className="flex">
+        <div>
+          <img src={product.image} alt="" width="500px " />
+        </div>
+        <div>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+          <h2>${product.price}</h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ProdeuctDetailPage;
