07839aa3b02151bcce069a5623a2eca9ea8f1028
My Name <aakarshpatel84@gmail.com>
2023-06-04 13:52:17 +0530

    Added View Button and implement Proper Alerts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setProducts } from "../../app/globalSlice";
 import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
 
 function Home() {
   const [categories, setCategories] = useState([]);
@@ -34,6 +35,7 @@ function Home() {
   const handleAddToCart = (product) => {
     // dispatch(addToCart(product));
     // product.quantity = 1;
+
     let isCheck = cartData.filter((el) => el.id === product.id);
     if (isCheck.length > 0) {
       alert("Product is already added");
@@ -41,6 +43,7 @@ function Home() {
       let newProduct = { ...product, quantity: 1 };
       cartData = [...cartData, newProduct];
       localStorage.setItem("cartData", JSON.stringify(cartData));
+      alert("product is Added");
     }
   };
   return (
@@ -76,33 +79,47 @@ function Home() {
           <div className="row gutter-2">
             {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
             {filteredProducts.map((item) => (
-              <Link
-                className="col-3 my-2"
-                style={{ textDecoration: "none", color: "black" }}
-                key={item.id}
-                to={`/product/${item.id}`}
-              >
-                <div>
-                  <div class="card h-100">
-                    <img
-                      class="card-img-top h-100"
-                      src={item.image}
-                      alt="Card cap"
-                    />
-                    <div class="card-body">
-                      <h5 class="card-title">{item.title}</h5>
-                      <p class="card-text">${item.price}</p>
+              <div className="col-3 my-2">
+                <div class="card h-100">
+                  <img
+                    class="card-img-top h-100"
+                    src={item.image}
+                    alt="Card cap"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">{item.title}</h5>
+                    <p class="card-text">${item.price}</p>
+                    <div
+                      style={{
+                        display: "flex",
+                        gap: "10px",
+                        justifyContent: "space-between",
+                      }}
+                    >
                       <button
                         href="#"
-                        class="btn btn-primary"
+                        class="btn btn-primary w-50"
                         onClick={() => handleAddToCart(item)}
                       >
                         Add to cart
                       </button>
+                      <Link
+                        style={{
+                          textDecoration: "none",
+                          color: "black",
+                          width: "90px",
+                        }}
+                        key={item.id}
+                        to={`/product/${item.id}`}
+                      >
+                        <button href="#" class="btn btn-secondary w-40">
+                          <FaEye />
+                        </button>
+                      </Link>
                     </div>
                   </div>
                 </div>
-              </Link>
+              </div>
             ))}
           </div>
         </div>
